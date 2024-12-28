a9ffe1a7bb3c5952e3ec1a8cc74a25379b949658
Moldirkab <moldirkabdykarim@gmail.com>
2024-12-28 22:18:18 +0600

    Added students.txt and teacher.txt files. Also, added try-catch block in the main part.

--- src/com/people/Main.java
+++ src/com/people/Main.java
@@ -2,53 +2,63 @@ package com.people;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        File fileStudents =new File("/Users/aidynkabdykarimov/Downloads/students.txt");
-        File fileTeachers =new File("/Users/aidynkabdykarimov/Downloads/teachers.txt");
+        File fileStudents =new File("students.txt");
+        File fileTeachers =new File("teachers.txt");
         Scanner scanner=new Scanner(fileStudents);
         Scanner scanner2=new Scanner(fileTeachers);
         School school=new School();
 
 
-        while (scanner.hasNextLine()) {
-            String name = scanner.next();
-            String surname = scanner.next();
-            int age = Integer.parseInt(scanner.next());
-            String gender = scanner.next();
-            boolean genderBool;
-            genderBool = gender.equals("Male");
-            Student student=new Student(name, surname, age, genderBool);
-            while(scanner.hasNextInt()){
-                student.addGrade(Integer.parseInt(scanner.next()));
+        try {
+            while (scanner.hasNextLine()) {
+                String name = scanner.next();
+                String surname = scanner.next();
+                int age = Integer.parseInt(scanner.next());
+                String gender = scanner.next();
+                boolean genderBool;
+                genderBool = gender.equals("Male");
+                Student student = new Student(name, surname, age, genderBool);
+                while (scanner.hasNextInt()) {
+                    student.addGrade(Integer.parseInt(scanner.next()));
+                }
+                school.addMember(student);
             }
-            school.addMember(student);
+
+        }catch(NoSuchElementException e){
+            System.out.println("Input is missing.");
         }
 
 
-        while (scanner2.hasNextLine()) {
-            String name = scanner2.next();
-            String surname = scanner2.next();
-            int age = Integer.parseInt(scanner2.next());
-            String gender = scanner2.next();
-            String subject=scanner2.next();
-            int yearsOfExperience = Integer.parseInt(scanner2.next());
-            int salary=Integer.parseInt(scanner2.next());
-            boolean genderBool;
-            genderBool = gender.equals("Male");
-            Teacher teacher=new Teacher(name, surname, age, genderBool, subject, yearsOfExperience, salary);
-            school.addMember(teacher);
+        try {
+            while (scanner2.hasNextLine()) {
+                String name = scanner2.next();
+                String surname = scanner2.next();
+                int age = Integer.parseInt(scanner2.next());
+                String gender = scanner2.next();
+                String subject = scanner2.next();
+                int yearsOfExperience = Integer.parseInt(scanner2.next());
+                int salary = Integer.parseInt(scanner2.next());
+                boolean genderBool;
+                genderBool = gender.equals("Male");
+                Teacher teacher = new Teacher(name, surname, age, genderBool, subject, yearsOfExperience, salary);
+                school.addMember(teacher);
+            }
+        }catch(NoSuchElementException e){
+            System.out.println("Input is missing.");
         }
 
 
         for(Person person: school.getMembers()){
-            if(person.getClass() == Student.class){
+            if(person instanceof Student){
                 Student student=(Student)person;
                 System.out.println(student.getName()+"'s GPA: " +String.format("%.2f",student.calculateGpa()));
             }
-            if(person.getClass() == Teacher.class){
+            if(person instanceof Teacher){
                 Teacher teacher=(Teacher)person;
                 if(teacher.getYearsOfExperience()>10){
                 System.out.println( teacher.getName()+"'s salary before rise: "+ teacher.getSalary()+ "."+"After the rise: "+teacher.giveRaise(20));
